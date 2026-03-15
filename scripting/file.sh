3c265e477e07564a08f5b9f6617572c1ca11a6f0
shriramhalyal2000 <shriramhalyal48@gmail.com>
2026-03-15 16:03:47 +0530

    new files

---
+++ scripting/file.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+dir=pwd
+echo "this is current working directory $dir"
+
+if [[ pwd == "/home/devops/bash-scripting/bash-scripting/scripting/" ]]; then
+  echo "correct directory"
+fi
---
+++ variables/var1.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "variables are use to store information that are used for later purpor, that can be repated again in cycle of logic"
