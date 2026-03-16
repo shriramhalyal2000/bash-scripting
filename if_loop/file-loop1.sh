e7a5a38620dda24acaea60ffdb818f8ec843d012
shriramhalyal2000 <shriramhalyal48@gmail.com>
2026-03-16 15:10:04 +0530

    checks file exists in dir or not

---
+++ if_loop/file-loop1.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# work_dir= "/home/shriram-halyal/devops/bash-scripting/bash-scripting"
+
+# sudo cd $work_dir
+# touch myfile.txt
+# echo "thos is script generated file" >> myfile.txt
+
+# if [ -f myfile.txt ]
+# then
+#     echo "file found, contents are:"
+#     cat myfile.txt | grep head -n 2
+# else
+#     echo "file not found"
+# fi
+
+# if [ -f myfile.txt ]
+# then
+#     rm -rf myfile.txt
+# fi
+
+# file=(find if_loop)
+# fi [ $file in . ]
+# then
+#     echo "file exists"
+# else
+#     echo "file doesnot exists"
+# fi
+
+pwd
+if [ -f ./if_loop/file-loop1.sh ]
+then
+    echo "file exists"
+else
+    echo "file doesnot exist in $pwd"
+fi
