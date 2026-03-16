e1fc29f07376ebddf5b7d933bc5f058ec321d773
shriramhalyal2000 <shriramhalyal48@gmail.com>
2026-03-16 15:15:00 +0530

    updated script to automat edir creation and deletion

--- if_loop/file-loop1.sh
+++ if_loop/file-loop1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+rmdir newDir
 # work_dir= "/home/shriram-halyal/devops/bash-scripting/bash-scripting"
 
 # sudo cd $work_dir
@@ -30,7 +30,17 @@
 pwd
 if [ -f ./if_loop/file-loop1.sh ]
 then
-    echo "file exists"
+    echo "file exists in $pwd"
 else
     echo "file doesnot exist in $pwd"
 fi
+
+echo "creating newDir"
+mkdir newDir
+find newDir
+echo "found newDir"
+cd newDir
+pwd
+cd ..
+echo "removing newDir"
+rmdir newDir
