3f7ec022f902b090ce62ebf02bfdbd96676aeedf
shriramhalyal2000 <shriramhalyal48@gmail.com>
2026-03-17 14:09:37 +0530

    added exit code examples

--- exitcodes/excode1.sh
+++ exitcodes/excode1.sh
@@ -9,6 +9,8 @@ then
     echo "Installation was scuessfule, exited withe code: $?"
     echo "to access the $package as follows"
     which $package
+    exit 0
 else
     echo "Installationwas unscuessful, exited with code: $?"
 fi
+exit 199
